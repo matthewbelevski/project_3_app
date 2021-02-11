e8dbd5a2b755bb3fad9e289750c6dbde2177616d
matthewbelevski <68624883+matthewbelevski@users.noreply.github.com>
2021-02-11 22:46:22 +1100

    added code

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+# load the model from disk
+filename = 'real_or_fake_model.pkl'
+#Loading Naive Bayes model
+news_model = pickle.load(open(filename, 'rb'))
+#count vectorizer
+cv = pickle.load(open('vectorizer.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = [str(x) for x in request.form.values()][0]
+		data = [message]
+        #data = cv.transform([message])
+        #vect = cv(data).toarray()
+		my_prediction = news_model.predict(cv.transform([message]))
+		if(my_prediction[0] == 1):
+    			output = "ITS A SPAM!"
+		elif(my_prediction[0] == 0):
+    			output = "NOT A SPAM"
+	return render_template('index.html',prediction_text = output)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
